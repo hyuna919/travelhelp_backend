aa183c9514e0195543e8cc746663c706c1d9d099
hyuna919 <hyuna919@naver.com>
2020-08-13 00:20:54 +0900

    index.js 코드정리

--- index.js
+++ index.js
@@ -2,17 +2,9 @@ const db = require('./conn_db.js');
 const app = require('./conn_android.js');
 var http = require('http');
 
-var getUserList = function(){
-    db.then(function(res){
-        console.log("----",res[0].id);
-    })
-}
-
-
 var server = http.createServer(app, function(req, res){
     res.writeHead(200,{'Content-Type':'text/html'});
 });
 
 server.listen(3000,function(){
-    getUserList();
 });
